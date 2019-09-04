911bc35bfbd6364280cb0104a6e2dc1c4d7e6f7e
tester73 <ahenestrosa@itba.edu.ar>
2019-09-04 15:58:40 -0300

    basico para slave process

---
+++ slaveProcess/slaveProcess.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+#define MAX_PATH_LENGTH 500
+
+int main(int argc, char * argv[]){
+
+    int pCount = argc, i, pid,error;
+    char * executeCommand = "minisat";     //Tener cuidado con los archivos con espacios
+    char * executeCommandArgs[3] = {"../satExamples/sat1.txt"  ,"../satExamples/satResults.txt",NULL};
+
+    for(i=1; i < 2 ; i++){
+        pid = fork();
+        if(-1 == pid){
+            perror("Error fork en slaveProcess: ");
+            return -1;
+        }
+
+        if(pid == 0){
+
+            error = execvp(executeCommand,executeCommandArgs);
+            if(error)
+                perror("Failed execvp in slave:");
+        }
+        else{
+            wait(&pCount);
+        }
+
+    }
+
+    if(pid != 0) printf("Termina el padre\n");
+    return 0;
+}
