c48134206f0d56606e081d5ba0efd8fbb36f839d
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2019-09-08 13:00:28 -0300

    Pipes

--- source/application.c
+++ source/application.c
@@ -8,6 +8,8 @@
 #include <dirent.h>
 
 #define SLAVE_COUNT 10
+#define WRITE_END 1
+#define READ_END 0
 
 int main(int argc, char * argv[]){
 
@@ -63,28 +65,51 @@ void sendFiles(const char * directory){
     }
 }
 
-int * createSlaves(int count, int ** sps){
+int * createSlaves(int count, int ** sp){
 	char * executeCommandArgs[4] = {"./slaveProcess.c","",NULL,NULL};
 	int slaves[count];
-	int * slavePipes[count];
+	int * pipesSlaveToMain[count];
+	int * pipesMainToSlave[count];
 	
 	int pid, error;
 
 	for (int i = 0; i < count; i++){
-		int slavePipe[2];	// 0 --> read end, 1 --> write end
+		int pipeToSlave[2];	// 0 --> read end, 1 --> write end
+		int pipeToMain[2];	// 0 --> read end, 1 --> write end
 
-		if (pipe(slavePipe) != 0){
-			perror("Error: ");
-		}
+		// Creo ambos pipes
+		if (pipe(pipeToSlave) != 0){ perror("Error: "); }
+		if (pipe(pipeToMain) != 0){ perror("Error: "); }
 
 		// Guardo el pipe para despues
-		slavePipes[i] = slavePipe;
+		pipesSlaveToMain[i] = pipeToMain;
+		pipesMainToSlave[i] = pipeToSlave;
 
 		pid = fork();
 
 		// En este caso es el hijo
 		if (pid == 0){
+
+			// Cierro el READ end del pipe que va hacia main
+			close(pipeToMain[READ_END])
+
+			// Cierro el WRITE end del pipe que va hacia el slave
+			close(pipeToSlave[WRITE_END])
+
+			// Le pongo el WRITE end del pipe que va al main en el STDOUT del slave
+			dup2( pipeToMain[WRITE_END], STDOUT_FILENO);
+
+			// Le pongo el READ end del pipe que va al slave en el STDIN del slave
+			dup2( pipeToSlave[READ_END], STDIN_FILENO);
+
+			// Cierro el WRITE end del pipe que va hacia main
+			close(pipeToMain[WRITE_END])
+
+			// Cierro el READ end del pipe que va hacia el slave
+			close(pipeToSlave[READ_END])
+
 			error = execvp(executeCommandArgs[0],executeCommandArgs);
+
 			if (error < 0){
 				perror("Error: ");
 			}
@@ -96,7 +121,7 @@ int * createSlaves(int count, int ** sps){
 		} 
 	}
 
-	sps = slavePipes;
+	(*sps) = (*slavePipes*);
 
 	return slaves;
 }
