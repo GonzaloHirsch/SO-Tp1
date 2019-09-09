d139b828c71429a1f0b4524017eade42fdc78536
Augusto <henestrosaaugusto@gmail.com>
2019-09-09 15:12:56 -0300

    Cambio del slave fozozo

--- source/slaveProcess.c
+++ source/slaveProcess.c
@@ -7,19 +7,18 @@
 #include <fcntl.h>
 
 
-#define MAX_PATH_LENGTH 500
+#define MAX_PROCESS_LENGTH 200
 #define UNDEFINED -1
 #define SAT 0
 #define UNSAT 1
+
+void analyseSatResults(char * processInfo, char * buffer, char * fileName);
 
 
 int main(int argc, char * argv[]){
 
     int  pCount, pid,error;
-    char * executeCommandArgs[4] = {"minisat","-verb=0",NULL,NULL};
-    char * results[2]= {"SATISFIABLE","UNSATISFIABLE"};
-    int result = -1;
-
+    char * executeCommandArgs[4] = {"minisat",NULL,NULL};
     //Para leer el resultado
     int pipefd[2]; //Descriptor para el redirigir el output del child.
 
@@ -30,7 +29,7 @@ int main(int argc, char * argv[]){
     // por un while(1) con un semaforo adentro.
     for(pCount=1; pCount < 2 ; pCount++){
 
-        char buffer[1024];  //en duda si ponerla aca o afuera.
+        char buffer[2048];  //en duda si ponerla aca o afuera.
         pipe(pipefd);//Creamos el canal de comunicacion entre el hijo y el padre
         pid = fork();
 
@@ -47,7 +46,7 @@ int main(int argc, char * argv[]){
             close(pipefd[1]);
 
             //Ejecutamos minisat
-            executeCommandArgs[2] = satEx;
+            executeCommandArgs[1] = satEx;
             error = execvp(executeCommandArgs[0],executeCommandArgs);
             if(error){
                 perror("Failed execvp in slave:");
@@ -56,72 +55,16 @@ int main(int argc, char * argv[]){
 
         }
         else{
-            wait(&pCount); //A modificar por waitpid
+            wait(&pCount); //A modificar por waitpid?
 
             //Leemos del hijo.
             close(pipefd[1]); //No necesitamos write end
             while (read(pipefd[0], buffer, sizeof(buffer)) != 0){}
 
-            //Chequeamos si es satisfacible o no.
-            //Vamos leyendo por todo el archivo hasta ver si encontramos si se imprimio
-            //que es satisfacible o insatisfacible. Esto es ya que a veces tira warnings y 
-            //mensajes extranios a pesar de estar en verbose=0 el programa minisat.
-            int i = 0,j = 1, state = UNDEFINED; result = UNDEFINED;
-            while(buffer[i] !=0 && result == UNDEFINED){
-                switch(state){
-                    case UNDEFINED:
-                        if(buffer[i] == results[SAT][0]){
-                            state = SAT;
-                        }
-                        else if(buffer[i] == results[UNSAT][0]){
-                            state = UNSAT;
-                        }
-                        break;
-                    case SAT:
-                        if(results[SAT][j] == 0){
-                            result = SAT;
-                        }
-                        else if(buffer[i] == results[SAT][j]){
-                            j++;
-                        }
-                        else if(buffer[i] == results[UNSAT][0]){ //probablemente innecesario
-                            state = UNSAT;
-                            j=1;
-                        }
-                        else{
-                            state =UNDEFINED;
-                            j=1;
-                        }
-                        break;
-                    case UNSAT:
-                        if(results[UNSAT][j] == 0){
-                            result = UNSAT;
-                        }
-                        else if(buffer[i] == results[UNSAT][j]){
-                            j++;
-                        }
-                        else if(buffer[i] == results[SAT][0]){
-                            state = SAT;
-                            j=1;
-                        }
-                        else{
-                            state =UNDEFINED;
-                            j=1;
-                        }
-                        break;
-                }
-                i++;
-            }
+            //Guardamos la informacion del proceso en processInfo
+            char processInfo[MAX_PROCESS_LENGTH];
+            analyseSatResults(processInfo, buffer,satEx);
 
-            if(result == SAT){
-                printf("Es satisfacible!\n");
-            }
-            else if(result == UNSAT){
-                printf("No es satifacible!\n");
-            }
-            else{
-                printf("Algo salio mal!\n");
-            }
 
         }
 
@@ -130,3 +73,56 @@ int main(int argc, char * argv[]){
     
     return 0;
 }
+
+void analyseSatResults(char * processInfo, char * buffer, char * fileName){
+
+    char * occurPosition;
+    char numberOfVariables[5];
+    char numberOfClauses[5];
+    char cpuTime[10];
+    char * satisfacible;
+
+    occurPosition = strstr(buffer, "Number of variables:");
+    if(occurPosition != NULL){
+        sscanf(occurPosition, "Number of variables: %s", numberOfVariables);
+    }
+    else{//Caso raro si no esta el dato.
+        perror("Failure finding the number of variables");
+    }
+
+    //Notar que para no reccorrer todo de nuevo arrancamos en occurposition del anterior.
+    occurPosition = strstr(occurPosition, "Number of clauses:");
+    if(occurPosition != NULL){
+        sscanf(occurPosition, "Number of clauses: %s",numberOfClauses);
+    }
+    else{ 
+        perror("Failure finding the number of clauses");
+    }
+    
+    occurPosition = strstr(occurPosition, "CPU time");
+    if(occurPosition != NULL){
+        sscanf(occurPosition, "CPU time : %s ",cpuTime);
+    }
+    else{
+        perror("Failure finding CPU time");
+    }
+
+    //Ahora verificamos si es satisfacible o no.
+    occurPosition = strstr(occurPosition,"UNSATISFIABLE");
+    if(occurPosition != NULL){
+        satisfacible = "UNSAT";
+    }
+    else if(strstr(buffer, "SATISFIABLE")!= NULL){ 
+        satisfacible = "SAT";
+    }
+    else{
+        perror("Error finding satisfacibility.");
+    }
+    
+    sprintf(processInfo,"%s\n%s\n%s\n%s\n%s\n%d\n", fileName, numberOfClauses,numberOfVariables,
+    satisfacible,cpuTime,getpid());
+
+    printf(processInfo);
+
+}
+
