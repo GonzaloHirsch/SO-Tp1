8c93b3acc321338c45611a7da2b4b3e58e421080
Augusto <henestrosaaugusto@gmail.com>
2019-09-10 10:33:14 -0300

    Mejora del slave
    Modularizacion y ahora recibe y devuelve por standard input/ooutput los archivos

--- source/slaveProcess.c
+++ source/slaveProcess.c
@@ -8,70 +8,101 @@
 
 
 #define MAX_PROCESS_LENGTH 200
-#define UNDEFINED -1
-#define SAT 0
-#define UNSAT 1
+#define MAX_INPUT_BUFFER 200
+#define MAX_SAT_INPUT 2048
 
+#define TERMINATE_MESSAGE "TERMINATE_PROCESS"
+
+#define INITIAL_PROCESSES 2
+
+int processSat(char * inputBuffer);
 void analyseSatResults(char * processInfo, char * buffer, char * fileName);
+void readFromStdin(char * inputBuffer, int size);
+void cleanBuffer(char * buffer, int size);
 
 
 int main(int argc, char * argv[]){
 
-    int  pCount, pid,error;
-    char * executeCommandArgs[4] = {"minisat",NULL,NULL};
-    //Para leer el resultado
-    int pipefd[2]; //Descriptor para el redirigir el output del child.
+    char inputBuffer[MAX_INPUT_BUFFER];
 
 
-    char * satEx = "../data/sat1.txt"; 
 
-    //Este ciclo for es unicamente represntativo, despues se va a cambiar
-    // por un while(1) con un semaforo adentro.
-    for(pCount=1; pCount < 2 ; pCount++){
+    readFromStdin(inputBuffer, MAX_INPUT_BUFFER);
+    while(strncmp(inputBuffer,TERMINATE_MESSAGE,sizeof(TERMINATE_MESSAGE) != 0)){
 
-        char buffer[2048];  //en duda si ponerla aca o afuera.
-        pipe(pipefd);//Creamos el canal de comunicacion entre el hijo y el padre
-        pid = fork();
-
-        if(-1 == pid){
-            perror("Error fork en slaveProcess: ");
-            return -1;
-        }
-        //El hijo que ejecutara el minisat
-        if(pid == 0){
-            
-            //Establecemos el IPC
-            close(pipefd[0]);
-            dup2(pipefd[1],STDOUT_FILENO);
-            close(pipefd[1]);
-
-            //Ejecutamos minisat
-            executeCommandArgs[1] = satEx;
-            error = execvp(executeCommandArgs[0],executeCommandArgs);
-            if(error){
-                perror("Failed execvp in slave:");
-                return -1;
-            }
-
-        }
-        else{
-            wait(&pCount); //A modificar por waitpid?
-
-            //Leemos del hijo.
-            close(pipefd[1]); //No necesitamos write end
-            while (read(pipefd[0], buffer, sizeof(buffer)) != 0){}
-
-            //Guardamos la informacion del proceso en processInfo
-            char processInfo[MAX_PROCESS_LENGTH];
-            analyseSatResults(processInfo, buffer,satEx);
-
-
-        }
+        //Creamos el esclavo y procesamos el sat.
+        //TO DO: ver que pasa si fallamos a procesar el minisat.
+        processSat(inputBuffer);
+    
+        //Limpiamos el buffer y leemos el input que nos manden.
+        cleanBuffer(inputBuffer,MAX_INPUT_BUFFER);
+        readFromStdin(inputBuffer,MAX_INPUT_BUFFER);        
 
     }
 
+
+
+    //TESTING
+    printf("Slave Terminated\n");
+
     
     return 0;
+}
+/*
+Creates a slave, procceses and parsed the info of the sat.
+Sends the info via stdout to application process.
+ */
+int processSat(char * inputBuffer){
+
+    int  pid,error,pCount;
+    char * executeCommandArgs[3] = {"minisat",NULL,NULL};
+    int pipefd[2]; //Descriptor para el redirigir el output del child.
+    char satBuffer[MAX_SAT_INPUT]; 
+
+    
+    pipe(pipefd);//Creamos el canal de comunicacion entre el hijo y el padre
+    pid = fork();
+
+    if(-1 == pid){
+        perror("Error fork en slaveProcess: ");
+        return -1;
+    }
+    //El hijo que ejecutara el minisat
+    if(pid == 0){
+        
+        //Establecemos el IPC con el hijo que va a ejecutar el minisat.
+        close(pipefd[0]);
+        dup2(pipefd[1],STDOUT_FILENO);
+        close(pipefd[1]);
+
+        //Ejecutamos minisat
+        executeCommandArgs[1] = inputBuffer;
+        error = execvp(executeCommandArgs[0],executeCommandArgs);
+        if(error){
+            perror("Failed executing minisat in slave:");
+            return -1;
+        }
+
+    }
+    else{ //TODO: fijarse como hacer por si falla el execvp del slave termine.
+
+        wait(&pCount); //A modificar por waitpid?
+
+        //Leemos del hijo.
+        close(pipefd[1]); //No necesitamos write end
+        while (read(pipefd[0], satBuffer, sizeof(satBuffer)) != 0){}
+
+        //Guardamos la informacion del proceso en processInfo
+        char processInfo[MAX_PROCESS_LENGTH];
+        analyseSatResults(processInfo, satBuffer,inputBuffer);
+
+        //Imprimimos en stdout
+        printf(processInfo);
+
+    }
+
+    return 0;
+
 }
 
 void analyseSatResults(char * processInfo, char * buffer, char * fileName){
@@ -122,7 +153,29 @@ void analyseSatResults(char * processInfo, char * buffer, char * fileName){
     sprintf(processInfo,"%s\n%s\n%s\n%s\n%s\n%d\n", fileName, numberOfClauses,numberOfVariables,
     satisfacible,cpuTime,getpid());
 
-    printf(processInfo);
 
 }
 
+
+
+void readFromStdin(char * inputBuffer, int size){
+    //Leemos de la stdin hasta que ingrese una newline.
+    char c = 0;
+    int cnt = 0;
+    while(read(STDIN_FILENO, &c, 1) != 0 && cnt < size - 1){
+        if(c == '\n'){
+            inputBuffer[cnt] = 0;
+            return;
+        }
+        inputBuffer[cnt++] = c;
+    }
+    inputBuffer[cnt] = 0; //Por las dudas, hay que asegurar que termine con 0.
+    return;
+}
+
+void cleanBuffer(char * buffer, int size){
+    int i=0;
+    for(;i<size;i++){
+        buffer[i] = 0;
+    }
+}
